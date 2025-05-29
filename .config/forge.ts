54965a7acff329befcd1f59887f8c9eaee0a911d
Yizack Rangel <yizackr@gmail.com>
2025-05-29 12:33:21 -0500

    feat: support macos and linux deb

--- .config/forge.ts
+++ .config/forge.ts
@@ -2,7 +2,7 @@ import type { ForgeConfig } from "@electron-forge/shared-types";
 import { MakerSquirrel } from "@electron-forge/maker-squirrel";
 import { MakerZIP } from "@electron-forge/maker-zip";
 import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
+import { MakerDMG } from "@electron-forge/maker-dmg";
 import { VitePlugin } from "@electron-forge/plugin-vite";
 import { FusesPlugin } from "@electron-forge/plugin-fuses";
 import { AutoUnpackNativesPlugin } from "@electron-forge/plugin-auto-unpack-natives";
@@ -31,7 +31,7 @@ export default {
   makers: [
     new MakerSquirrel({ usePackageJson: true }),
     new MakerZIP({}),
-    new MakerRpm({}),
+    new MakerDMG({}),
     new MakerDeb({})
   ],
   plugins: [
